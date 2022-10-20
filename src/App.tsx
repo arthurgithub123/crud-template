c630f29c1e451c9b6a9c8461ff48b8ba0f17b2c3
Arthur <arthuromp000@gmail.com>
2022-10-20 17:29:25 -0300

    Ident code

--- src/App.tsx
+++ src/App.tsx
@@ -55,113 +55,127 @@ const App: React.FC = () => {
         </Menu>
       </Header>
       <Layout>
-      <Sider
-        collapsible
-        onCollapse={() => {
-          if(mainRef.current && footerRef.current) {
-            let collapsedValue = !collapsed;
+        <Sider
+          collapsible
+          onCollapse={() => {
+            if (mainRef.current && footerRef.current) {
+              let collapsedValue = !collapsed;
 
-            if(collapsedValue) {
-              mainRef.current.style.transition   = 'margin 0.2s ease';
-              mainRef.current.style.marginTop    = '80px';
-              mainRef.current.style.marginRight  = '16px';
-              mainRef.current.style.marginBottom = '10px';
-              mainRef.current.style.marginLeft   = '96px';
-              footerRef.current.style.transition = 'margin 0.2s ease';
-              footerRef.current.style.marginLeft = '80px';
-            }
-            else {
-              mainRef.current.style.transition   = 'margin 0.2s ease';
-              mainRef.current.style.marginTop    = '80px';
-              mainRef.current.style.marginRight  = '16px';
-              mainRef.current.style.marginBottom = '10px';
-              mainRef.current.style.marginLeft   = '216px';
-              footerRef.current.style.transition = 'margin 0.2s ease';
-              footerRef.current.style.marginLeft = '200px';
-            }
-
-            setCollapsed(!collapsed);
-          }
-      }}
-        style={{
-          height: '83vh',
-          position: 'fixed',
-          top: '64px',
-          left: '0px',
-          overflow: 'auto',
-          scrollbarWidth: 'thin',
-        }}
-      >
-        <div className="logo" />
-        <Menu theme="dark" 
-          defaultSelectedKeys={['key1']}
-          selectedKeys={['key1']} 
-          mode="inline"
-          style={{ height: '100%' }}
-        >
-          <Menu.Item key="key1" icon={<PieChartOutlined />}>
-            <span>1 Products</span>
-          </Menu.Item>
-          <Menu.Item key="key2" icon={<PieChartOutlined />}>
-            <span>2 Categories</span>
-          </Menu.Item>
-          <Menu.Item key="key3" icon={<PieChartOutlined />}>
-            <span>3 Suppliers</span>
-          </Menu.Item>
-          <Menu.Item key="key4" icon={<PieChartOutlined />}>
-            <span>4</span>
-          </Menu.Item>
-          <Menu.Item key="key5" icon={<PieChartOutlined />}>
-            <span>5</span>
-          </Menu.Item>
-          <Menu.Item key="key6" icon={<PieChartOutlined />}>
-            <span>6</span>
-          </Menu.Item>
-          <Menu.Item key="key7" icon={<PieChartOutlined />}>
-            <span>7</span>
-          </Menu.Item>
-          <Menu.Item key="key8" icon={<PieChartOutlined />}>
-            <span>8</span>
-          </Menu.Item>
-          <Menu.Item key="key9" icon={<PieChartOutlined />}>
-            <span>9</span>
-          </Menu.Item>
-          <Menu.Item key="key10" icon={<PieChartOutlined />}>
-            <span>10</span>
-          </Menu.Item>
-          <Menu.Item key="key11" icon={<PieChartOutlined />}>
-            <span>11</span>
-          </Menu.Item>
-          <Menu.Item key="key12" icon={<PieChartOutlined />}>
-            <span>12</span>
-          </Menu.Item>
-          <Menu.Item key="key13" icon={<PieChartOutlined />}>
-            <span>13</span>
-          </Menu.Item>
-          <Menu.Item key="key14" icon={<PieChartOutlined />}>
-            <span>14</span>
-          </Menu.Item>
-          <Menu.Item key="key15" icon={<PieChartOutlined />}>
-            <span>15</span>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Content ref={mainRef} style={{ marginTop: '80px', marginRight: '16px', marginBottom: '10px', marginLeft: '216px' }}> {/* style={{ margin: "16px 16px", background: "#fff", marginLeft: '200px', marginTop: '64px' }} */}
-          <div style={{ padding: 24, minHeight: 360 }}>
-            <span>some item</span>
-              {  Array.from({ length: 100 }, (_, index) => (
-                  <span key={index}>
-                    {index % 20 === 0 && index ? 'more' : '...'}
-                    <br />
-                  </span>
-                ))
+              if (collapsedValue) {
+                mainRef.current.style.transition = 'margin 0.2s ease';
+                mainRef.current.style.marginTop = '80px';
+                mainRef.current.style.marginRight = '16px';
+                mainRef.current.style.marginBottom = '10px';
+                mainRef.current.style.marginLeft = '96px';
+                footerRef.current.style.transition = 'margin 0.2s ease';
+                footerRef.current.style.marginLeft = '80px';
               }
-          </div>
-        </Content>
-        <Footer ref={footerRef} style={{ textAlign: 'center', marginLeft: '200px' }}>Copyrights &copy; 2020 All Rights Reseverd by Company.</Footer>
+              else {
+                mainRef.current.style.transition = 'margin 0.2s ease';
+                mainRef.current.style.marginTop = '80px';
+                mainRef.current.style.marginRight = '16px';
+                mainRef.current.style.marginBottom = '10px';
+                mainRef.current.style.marginLeft = '216px';
+                footerRef.current.style.transition = 'margin 0.2s ease';
+                footerRef.current.style.marginLeft = '200px';
+              }
+
+              setCollapsed(!collapsed);
+            }
+          }}
+          style={{
+            height: '83vh',
+            position: 'fixed',
+            top: '64px',
+            left: '0px',
+            overflow: 'auto',
+            scrollbarWidth: 'thin',
+          }}
+        >
+          <div className="logo" />
+          <Menu theme="dark"
+            defaultSelectedKeys={['key1']}
+            selectedKeys={['key1']}
+            mode="inline"
+            style={{ height: '100%' }}
+          >
+            <Menu.Item key="key1" icon={<PieChartOutlined />}>
+              <span>1 Products</span>
+            </Menu.Item>
+            <Menu.Item key="key2" icon={<PieChartOutlined />}>
+              <span>2 Categories</span>
+            </Menu.Item>
+            <Menu.Item key="key3" icon={<PieChartOutlined />}>
+              <span>3 Suppliers</span>
+            </Menu.Item>
+            <Menu.Item key="key4" icon={<PieChartOutlined />}>
+              <span>4</span>
+            </Menu.Item>
+            <Menu.Item key="key5" icon={<PieChartOutlined />}>
+              <span>5</span>
+            </Menu.Item>
+            <Menu.Item key="key6" icon={<PieChartOutlined />}>
+              <span>6</span>
+            </Menu.Item>
+            <Menu.Item key="key7" icon={<PieChartOutlined />}>
+              <span>7</span>
+            </Menu.Item>
+            <Menu.Item key="key8" icon={<PieChartOutlined />}>
+              <span>8</span>
+            </Menu.Item>
+            <Menu.Item key="key9" icon={<PieChartOutlined />}>
+              <span>9</span>
+            </Menu.Item>
+            <Menu.Item key="key10" icon={<PieChartOutlined />}>
+              <span>10</span>
+            </Menu.Item>
+            <Menu.Item key="key11" icon={<PieChartOutlined />}>
+              <span>11</span>
+            </Menu.Item>
+            <Menu.Item key="key12" icon={<PieChartOutlined />}>
+              <span>12</span>
+            </Menu.Item>
+            <Menu.Item key="key13" icon={<PieChartOutlined />}>
+              <span>13</span>
+            </Menu.Item>
+            <Menu.Item key="key14" icon={<PieChartOutlined />}>
+              <span>14</span>
+            </Menu.Item>
+            <Menu.Item key="key15" icon={<PieChartOutlined />}>
+              <span>15</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Content
+            ref={mainRef}
+            style={{
+              marginTop: '80px',
+              marginRight: '16px',
+              marginBottom: '10px',
+              marginLeft: '216px'
+            }}> {/* style={{ margin: "16px 16px", background: "#fff", marginLeft: '200px', marginTop: '64px' }} */}
+            <div style={{ padding: 24, minHeight: 360 }}>
+              <span>some item</span>
+              {Array.from({ length: 100 }, (_, index) => (
+                <span key={index}>
+                  {index % 20 === 0 && index ? 'more' : '...'}
+                  <br />
+                </span>
+              ))
+              }
+            </div>
+          </Content>
+          <Footer
+            ref={footerRef}
+            style={{
+              textAlign: 'center',
+              marginLeft: '200px'
+            }}
+          >
+            Copyrights &copy; 2020 All Rights Reseverd by Company.</Footer>
+        </Layout>
       </Layout>
-    </Layout>
     </Layout>
   );
 }
